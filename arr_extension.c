9a8514ef31cf6499039f88601d8b481e7ba64559
johnnye <johnny@johnny.com>
2017-12-18 10:39:36 +0800

    update

--- arr_extension.c
+++ arr_extension.c
@@ -12,7 +12,8 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author:                                                              |
+  | Author:  johnny                                                      |
+  | Email :  leiyuqing_jing@163.com										 |
   +----------------------------------------------------------------------+
 */
 
